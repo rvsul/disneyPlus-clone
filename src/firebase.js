123b9b78e6602d96974110321444a87ef071604b
Rasul Agasiyev <79022070+rvsul@users.noreply.github.com>
2022-07-24 19:09:55 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import "firebase/compat/firestore";
 import "firebase/compat/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB09TIIvEHLNIYVP06CEFay_zngXqSBMyU",
-  authDomain: "disney-clone-2bd67.firebaseapp.com",
-  projectId: "disney-clone-2bd67",
-  storageBucket: "disney-clone-2bd67.appspot.com",
-  messagingSenderId: "132597009252",
-  appId: "1:132597009252:web:de4361b79ef3cb10fe0871",
-  measurementId: "G-QT1PMPXW55",
+  apiKey: "AIzaSyAx3mtMla7UF1OeF_0vuzQRc1t_Vy3optE",
+  authDomain: "disneyplus-clone-81676.firebaseapp.com",
+  projectId: "disneyplus-clone-81676",
+  storageBucket: "disneyplus-clone-81676.appspot.com",
+  messagingSenderId: "122901957319",
+  appId: "1:122901957319:web:752631ff9d64a0e4ed4ce2",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
